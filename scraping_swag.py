073b2816545e7c8f69726f2d59b82633ca971d0a
arthur <afariagoncalves72@gmail.com>
2025-05-20 11:03:59 -0300

    adiciona funcao raspa_produto e implementa em coleta_dados

--- scraping_swag.py
+++ scraping_swag.py
@@ -1,51 +1,71 @@
 from seleniumbase import SB
 
-dados = {"mochila": [], 
-          "lanterna": [], 
-          "camisa preta": [], 
-          "casaco": [], 
-          "macacão": [],
-          "camisa laranja": []}
+
 
 with SB(browser="chrome") as sb:
 
+    def raspa_produto(seletor_nome, seletor_descricao, seletor_preco):
+
+        produto = []
+
+        nome_elemento = sb.find_element(seletor_nome)
+        nome = nome_elemento.text
+        produto.append(nome)
+
+        descricao_elemento = sb.find_element(seletor_descricao)
+        descricao = descricao_elemento.text
+        produto.append(descricao)
+
+        preco_elemento = sb.find_element(seletor_preco)
+        preco = preco_elemento.text
+        produto.append(preco)
+
+        return produto
+        
     def login():
         sb.open("https://www.saucedemo.com")    
         sb.type("#user-name", "standard_user")  
         sb.type("#password", "secret_sauce\n")
 
     def coleta_dados():
-        sb.assert_element('div:contains("Sauce Labs Backpack")')
+        sb.wait_for_element('.app_logo')
         
-        #mochila
-        dados["mochila"].append(sb.get_text("#item_4_title_link > div:nth-child(1)"))
-        dados["mochila"].append(sb.get_text("div.inventory_item:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)"))
-        dados["mochila"].append(sb.get_text("div.inventory_item:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)"))
+        dados["mochila"] = raspa_produto("#item_4_title_link > div:nth-child(1)",
+                                         "div.inventory_item:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)",
+                                         "div.inventory_item:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)")
+        
+        dados["lanterna"] = raspa_produto("#item_0_title_link > div:nth-child(1)",
+                                          "div.inventory_item:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)",
+                                          "div.inventory_item:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)")
+        
+        dados["camisa preta"] = raspa_produto("#item_1_title_link > div:nth-child(1)",
+                                              "div.inventory_item:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)",
+                                              "div.inventory_item:nth-child(3) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)")
+        
+        dados["casaco"] = raspa_produto("#item_5_title_link > div:nth-child(1)",
+                                        "div.inventory_item:nth-child(4) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)",
+                                        "div.inventory_item:nth-child(4) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)")
+        
+        dados["macacão"] = raspa_produto("#item_2_title_link > div:nth-child(1)",
+                                         "div.inventory_item:nth-child(5) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)",
+                                         "div.inventory_item:nth-child(5) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)")
+        
+        dados["camisa laranja"] = raspa_produto("#item_3_title_link > div:nth-child(1)",
+                                                "div.inventory_item:nth-child(6) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)",
+                                                "div.inventory_item:nth-child(6) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)")
 
-        #lanterna
-        dados["lanterna"].append(sb.get_text("#item_0_title_link > div:nth-child(1)"))
-        dados["lanterna"].append(sb.get_text("div.inventory_item:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)"))
-        dados["lanterna"].append(sb.get_text("div.inventory_item:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)"))
-        
-        #camisa preta
-        dados["camisa preta"].append(sb.get_text("#item_1_title_link > div:nth-child(1)"))
-        dados["camisa preta"].append(sb.get_text("div.inventory_item:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)"))
-        dados["camisa preta"].append(sb.get_text("div.inventory_item:nth-child(3) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)"))
+    dados = {"mochila": [], 
+          "lanterna": [], 
+          "camisa preta": [], 
+          "casaco": [], 
+          "macacão": [],
+          "camisa laranja": []}
     
-        #casaco
-        dados["casaco"].append(sb.get_text("#item_5_title_link > div:nth-child(1)"))
-        dados["casaco"].append(sb.get_text("div.inventory_item:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)"))   
-        dados["casaco"].append(sb.get_text("div.inventory_item:nth-child(4) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)"))
-   
-        #macacão
-        dados["macacão"].append(sb.get_text("#item_2_title_link > div:nth-child(1)"))
-        dados["macacão"].append(sb.get_text("div.inventory_item:nth-child(5) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)"))
-        dados["macacão"].append(sb.get_text("div.inventory_item:nth-child(5) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)"))
-        
-        #camisa laranja
-        dados["camisa laranja"].append(sb.get_text("#item_3_title_link > div:nth-child(1)"))
-        dados["camisa laranja"].append(sb.get_text("div.inventory_item:nth-child(6) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)"))
-        dados["camisa laranja"].append(sb.get_text("div.inventory_item:nth-child(6) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)"))
-
     login()
     coleta_dados()
+    print(dados["mochila"])
+    print(dados["lanterna"])
+    print(dados["camisa preta"])
+    print(dados["casaco"])
+    print(dados["macacão"])
+    print(dados["camisa laranja"])
